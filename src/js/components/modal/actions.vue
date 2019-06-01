04e71db1e40c10b64ccb70295202bb4d9ad8bc26
Adam Mikacich <adammikacich@gmail.com>
2019-06-01 13:37:25 -0700

    Disable ability to register or deregister on locked events

--- src/js/components/modal/actions.vue
+++ src/js/components/modal/actions.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="actions">
     <button class="btn" id="view" @click="view">{{ $store.state.view == 1 ? 'View List' : 'View Info'}}</button>
-    <button class="btn" id="register" @click="register">{{ $store.getters.currentEvent.registered ? 'Deregister' : 'Register'}}</button>
+    <button class="btn" id="register" @click="register">
+      {{
+        !canRegister
+        ? 'Locked'
+        : $store.getters.currentEvent.registered ? 'Deregister' : 'Register'
+      }}
+    </button>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    canRegister: function() {
+      return !this.$store.getters.currentEvent.classes.includes('locked');
+    }
+  },
   methods: {
     view: function() {
       this.$store.commit('view', {view: this.$store.state.view == 1 ? 2 : 1});
     },
     register: function() {
-      this.$store.commit('changeRegister');
+      if (this.canRegister) this.$store.commit('changeRegister');
     }
   }
 };
